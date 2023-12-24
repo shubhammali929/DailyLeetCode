733b6dbe244ad15e7193f0d6ae73ddf0df2da568
shubhammali929 <shubhammali929@gmail.com>
2023-12-24 23:41:07 +0530

    24-DEC

---
+++ MergeTwoSortedLists.java
@@ -0,0 +1,15 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
