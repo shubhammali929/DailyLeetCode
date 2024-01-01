ce14a134f191ff7613140a707ad76f0158469d66
shubhammali929 <shubhammali929@gmail.com>
2024-01-01 23:30:23 +0530

    01-Jan

--- IsomorphicStrings205.java
+++ IsomorphicStrings205.java
@@ -13,10 +13,33 @@ public class IsomorphicStrings205 {
                     return false;
             }
         }
+        Map<Character, Character> mp2 = new HashMap<>();
+        for(int i=0; i<s.length(); i++)
+        {
+            if(! mp2.containsKey(t.charAt(i))){
+                mp2.put(t.charAt(i),s.charAt(i));
+            }else{
+                if(mp2.get(t.charAt(i))!=s.charAt(i))
+                    return false;
+            }
+        }
         // System.out.println(mp);
-        return true;
+        // System.out.println(mp2);
+        String news ="";
+        String news2 = "";
+        for(int i=0; i<s.length(); i++){
+            news += mp.get(s.charAt(i));
+        }
+        for(int i=0; i<s.length(); i++){
+            news2 += mp2.get(t.charAt(i));
+        }
+        // System.out.println("Result-->"+news);
+        // System.out.println("Result2-->"+news2);
+        if(news.equals(t) && news2.equals(s))
+            return true;
+        return false;
     }
     public static void main(String[] args) {
-        System.out.println(isIsomorphic("Apple","Alljq"));
+        System.out.println(isIsomorphic("egg","add"));
     }
 }
