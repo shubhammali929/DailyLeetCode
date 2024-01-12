1064a71e0bcb4dc73f517186f40cd376b056a85e
shubhammali929 <shubhammali929@gmail.com>
2024-01-12 15:10:19 +0530

    12-Jan

--- FindFirstAndLastPositionOfElement34.java
+++ FindFirstAndLastPositionOfElement34.java
@@ -1,39 +1,26 @@
 // pending ...
+// Solved ..✔️
 public class FindFirstAndLastPositionOfElement34 {
     public static int[] searchRange(int[] nums, int target) {
+        int[] arr2 = {0,0};
+        if(nums.length == 1 && nums[0]==target)
+            return arr2;
         int start = -1;
         int end = -1;
-        int i = 0;
-        int j = nums.length - 1;
-        int count = 1;
-        while (count <= nums.length) {
-            // Added braces for better structure
-            if (nums[i] == target) {
-                if (start == -1) {
-                    start = i;
-                } else {
-                    i++;
-                }
-            }
-
-            // Added braces for better structure
-            if (nums[j] == target) {
-                if (end == -1) {
-                    end = j;
-                } else {
-                    j--;
-                }
-            }
-
-            count++;
+        for(int i=0; i<nums.length; i++){
+            if(start == -1 && nums[i] == target)
+                start = end=i;
+            else if(start != -1 && nums[i]==target)
+                end = i;
         }
-        int[] res = {start, end};
-        return res;
+        int[] arr = {start, end};
+        return arr;
     }
 
     public static void main(String[] args) {
-        int[] arr = {5, 7, 7, 8, 8, 10};
-        int[] res = searchRange(arr, 8);
+        // int[] arr = {5, 7, 7, 8, 8, 10};
+        int[] arr = {1,3};
+        int[] res = searchRange(arr, 1);
         for (int i = 0; i < res.length; i++)
             System.out.print(res[i] + " ");
     }
