cd9f697e0a16dd5a31ef6063f7a2f991f7494224
shubhammali929 <shubhammali929@gmail.com>
2024-01-22 22:57:38 +0530

    22-Jan

--- ArrayPartition561.java
+++ ArrayPartition561.java
@@ -6,7 +6,6 @@ public class ArrayPartition561 {
             sum+=nums[i];
         }
         return sum;
-
     }
     public static void main(String[] args) {
         int[] arr = {6,2,6,5,1,2}; //[1,2,2,5,6,6] after sorted -> [min(1,2) + min(2,5) + min(6,6)]
