d827cfac0e5f0d969ff57d0089e81c8cf67b8229
shubhammali929 <shubhammali929@gmail.com>
2024-02-01 23:56:28 +0530

    01-Feb

--- PascalsTriangle118.java
+++ PascalsTriangle118.java
@@ -1,4 +1,15 @@
+import java.util.*;
+
 public class PascalsTriangle118 {
+    public static List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> aList = new ArrayList<List<Integer>>();
+        for (int i=0;i<numRows;++i){
+            List<Integer> list = new ArrayList<>();
+            for(int j=0; j<i; j++){
+                list.
+            }
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello");
     }
