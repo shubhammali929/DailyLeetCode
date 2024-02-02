93365749ed7b5816a9e0dd5c47837cbdd4e8c479
shubhammali929 <shubhammali929@gmail.com>
2024-02-02 12:09:30 +0530

    Update generate method to handle more than 1 row in Pascal's Triangle (generated by blackbox.ai )

--- PascalsTriangle118.java
+++ PascalsTriangle118.java
@@ -1,6 +1,7 @@
 import java.util.*;
-
 public class PascalsTriangle118 {
+
+
     public static List<List<Integer>> generate(int numRows) {
         List<List<Integer>> list = new ArrayList<>();
 
@@ -8,12 +9,16 @@ public class PascalsTriangle118 {
         List<Integer> firstRow = new ArrayList<>();
         firstRow.add(1);
         list.add(firstRow);
+
+        if(numRows==1) return list;
+
+
         for (int i=1;i<numRows;i++){
 
             List<Integer> prev = list.get(i-1);
             List<Integer> row = new ArrayList<>();
             row.add(1);
-            for(int j=0; j<i; j++){
+            for(int j=0; j<i-1; j++){
                 int sum = prev.get(j)+prev.get(j+1);
                 row.add(sum);
             }
@@ -24,6 +29,6 @@ public class PascalsTriangle118 {
     }
     public static void main(String[] args) {
         System.out.println("Hello");
-        System.out.println(generate(3));
+        System.out.println(generate(5));
     }
 }
